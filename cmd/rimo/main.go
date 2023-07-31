30ec3f7b30f46e200b3893731354b400625d1475
mathisdrn <116900975+mathisdrn@users.noreply.github.com>
2023-07-31 09:19:11 +0000

    refactor: analyse command

--- cmd/rimo/main.go
+++ cmd/rimo/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/cgi-fr/rimo/pkg/rimo"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -26,23 +28,32 @@ func main() {
 
 	rootCmd := &cobra.Command{ //nolint:exhaustruct
 		Use:   "rimo",
-		Short: "Tools to help generate PIMO masking",
+		Short: "Series of tool to help generate PIMO masking",
 		Version: fmt.Sprintf(`%v (commit=%v date=%v by=%v)
-		// Copyright (C) 2021 CGI France
-		// License GPLv3: GNU GPL version 3 <https://gnu.org/licenses/gpl.html>.
-		// This is free software: you are free to change and redistribute it.
+		Copyright (C) 2021 CGI France
+		License GPLv3: GNU GPL version 3 <https://gnu.org/licenses/gpl.html>.
+		This is free software: you are free to change and redistribute it.
 		There is NO WARRANTY, to the extent permitted by law.`, version, commit, buildDate, builtBy),
+	}
+
+	analyseCmd := &cobra.Command{ //nolint:exhaustruct
+		Use:   "analyse [input_path] [output_path]",
+		Short: "Analyse a jsonl file and output a yaml file",
 		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello World!")
+			inputPath := args[0]
+			outputPath := args[1]
+			CheckFile(inputPath)
+			CheckFile(outputPath)
+
+			var inputList []string
+			var outputList []string
+
+			rimo.Analyse(inputList, outputList)
 		},
 	}
 
-	rootCmd.AddCommand(&cobra.Command{ //nolint:exhaustruct
-		Use:   "analyse",
-		Short: "Command line to analyse jsonl file",
-		Run: func(cmd *cobra.Command, args []string) {
-			helloWorld()
-		},
-	})
+	rootCmd.AddCommand(analyseCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Err(err).Msg("Error when executing command")
@@ -50,7 +61,16 @@ func main() {
 	}
 }
 
-func helloWorld() {
-	fmt.Println("Hello World!")
-	log.Print("Hello World!")
+func CheckFile(path string) {
+	fileInfo, err := os.Stat(path)
+
+	absPath, _ := filepath.Abs(path)
+	// Check if the file exists
+	if os.IsNotExist(err) {
+		log.Fatalf("file does not exist: %s", absPath)
+	}
+	// Check if the file is a regular file
+	if !fileInfo.Mode().IsRegular() {
+		log.Fatalf("not a regular file: %s", absPath)
+	}
 }
