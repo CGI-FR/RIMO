2ede8adf4ac66021b377a3f164fb9e45dc07a95e
mathisdrn <116900975+mathisdrn@users.noreply.github.com>
2023-07-31 09:19:45 +0000

    feat: analyse.go orchestrator of analyse package

--- pkg/analyse/analyse.go
+++ pkg/analyse/analyse.go
@@ -4,7 +4,44 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/cgi-fr/rimo/pkg/models"
 )
+
+type DataCol struct {
+	BaseName  string
+	TableName string
+	ColName   string
+	ColType   string
+	Values    []interface{}
+}
+
+// Handle execution pipeline of analyse pkg.
+func Analyse(inputList []string, outputPath string) {
+	base := make(models.Base)
+
+	// Iterate over inputList and outputList.
+	// Load, Analyse and Export.
+	for i := range inputList {
+		inputPath := inputList[i]
+		// Extract Base and Table name from inputFilePath.
+		baseName := filepath.Base(filepath.Dir(inputPath))
+		tableName := filepath.Base(inputPath)
+
+		// Load inputFilePath.
+		data := Load(inputPath)
+
+		// Analyse
+		for dataColumn := range data {
+			// Append each column to Base structure.
+			dataTable := ComputeMetric(dataColumn)
+			base[baseName][tableName] = dataTable
+		}
+	}
+
+	// Export
+	Export(base, outputPath)
+}
 
 func CheckFile(path string) {
 	fileInfo, err := os.Stat(path)
