bed9e305d7c26bff3fb983482e5f676f05152637
mathisdrn <116900975+mathisdrn@users.noreply.github.com>
2023-08-09 21:19:29 +0000

    feat: added rimo schema

--- go.mod
+++ go.mod
@@ -13,10 +13,11 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/invopop/jsonschema v0.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/mod v0.12.0 // indirect
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,53 +1,98 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/invopop/jsonschema"
+)
+
 // RIMO YAML structure.
 type (
 	Base struct {
-		Name   string  `yaml:"database"`
-		Tables []Table `yaml:"tables"`
+		Name   string  `yaml:"database" json:"database" jsonschema:"required"`
+		Tables []Table `yaml:"tables" json:"tables" jsonschema:"required"`
 	}
+
 	Table struct {
-		Name    string   `yaml:"name"`
-		Columns []Column `yaml:"columns"`
+		Name    string   `yaml:"name" json:"name" jsonschema:"required"`
+		Columns []Column `yaml:"columns" json:"columns" jsonschema:"required"`
 	}
+
 	Column struct {
-		Name         string   `yaml:"name"`
-		Type         string   `yaml:"type"`
-		Concept      string   `yaml:"concept"`
-		Constraint   []string `yaml:"constraint"`
-		Confidential *bool    `yaml:"confidential"`
+		Name         string        `yaml:"name" json:"name" jsonschema:"required"`
+		Type         string        `yaml:"type" json:"type" jsonschema:"required"`
+		Concept      string        `yaml:"concept" json:"concept" jsonschema:"required"`
+		Constraint   []string      `yaml:"constraint" json:"constraint" jsonschema:"required"`
+		Confidential *bool         `yaml:"confidential" json:"confidential" jsonschema:"required"`
+		MainMetric   GenericMetric `yaml:"mainMetric" json:"mainMetric" jsonschema:"required"`
 
-		MainMetric GenericMetric `yaml:"mainMetric"`
-
-		StringMetric  StringMetric  `yaml:"stringMetric,omitempty"`
-		NumericMetric NumericMetric `yaml:"numericMetric,omitempty"`
-		BoolMetric    BoolMetric    `yaml:"boolMetric,omitempty"`
+		StringMetric  StringMetric  `yaml:"stringMetric,omitempty" json:"stringMetric,omitempty"`
+		NumericMetric NumericMetric `yaml:"numericMetric,omitempty" json:"numericMetric,omitempty"`
+		BoolMetric    BoolMetric    `yaml:"boolMetric,omitempty" json:"boolMetric,omitempty"`
 	}
 )
 
 // RIMO YAML metrics.
 type (
 	GenericMetric struct {
-		Count  int64         `yaml:"count"`
-		Unique int64         `yaml:"unique"`
-		Sample []interface{} `yaml:"sample"`
+		Count  int64         `yaml:"count" json:"count" jsonschema:"required"`
+		Unique int64         `yaml:"unique" json:"unique" jsonschema:"required"`
+		Sample []interface{} `yaml:"sample" json:"sample" jsonschema:"required"`
 	}
 	StringMetric struct {
-		MostFreqLen     []LenFreq `yaml:"mostFrequentLen"`
-		LeastFreqLen    []LenFreq `yaml:"leastFrequentLen"`
-		LeastFreqSample []string  `yaml:"leastFrequentSample"`
+		MostFreqLen     []LenFreq `yaml:"mostFrequentLen" json:"mostFrequentLen" jsonschema:"required"`
+		LeastFreqLen    []LenFreq `yaml:"leastFrequentLen" json:"leastFrequentLen" jsonschema:"required"`
+		LeastFreqSample []string  `yaml:"leastFrequentSample" json:"leastFrequentSample" jsonschema:"required"`
 	}
 
 	LenFreq struct {
-		Length int
-		Freq   float64
+		Length int     `json:"length" jsonschema:"required"`
+		Freq   float64 `json:"freq" jsonschema:"required"`
 	}
+
 	NumericMetric struct {
-		Min  float64 `yaml:"min"`
-		Max  float64 `yaml:"max"`
-		Mean float64 `yaml:"mean"`
+		Min  float64 `yaml:"min" json:"min" jsonschema:"required"`
+		Max  float64 `yaml:"max" json:"max" jsonschema:"required"`
+		Mean float64 `yaml:"mean" json:"mean" jsonschema:"required"`
 	}
+
 	BoolMetric struct {
-		TrueRatio float64 `yaml:"trueRatio"`
+		TrueRatio float64 `yaml:"trueRatio" json:"trueRatio" jsonschema:"required"`
 	}
 )
+
+func main() {
+	err := ExportBaseSchema()
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+}
+
+// ExportBaseSchema exports the YAML schema for the Base struct to the current folder.
+func ExportBaseSchema() error {
+	// Marshal the struct into a JSON string
+	s := jsonschema.Reflect(&Base{}) //nolint:exhaustruct
+
+	schema, err := json.MarshalIndent(s, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON schema: %w", err)
+	}
+
+	// Create a new file for the JSON schema
+	file, err := os.Create("rimo-schema.json")
+	if err != nil {
+		return fmt.Errorf("error creating JSON schema file: %w", err)
+	}
+	defer file.Close()
+
+	// Write the JSON data to the file
+	_, err = file.Write(schema)
+	if err != nil {
+		return fmt.Errorf("error writing JSON data to file: %w", err)
+	}
+
+	return nil
+}
