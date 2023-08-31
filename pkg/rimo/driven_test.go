257d7ef06e10e82297763912b75cb25cfac04af8
mathisdrn <mathis.derenne@gmail.com>
2023-08-31 08:25:57 +0000

    feat(rimo): driven_test.go

--- pkg/rimo/driven_test.go
+++ pkg/rimo/driven_test.go
@@ -1,28 +1,139 @@
 package rimo_test
 
 import (
+	"log"
+	"math"
+	"testing"
+
+	"github.com/cgi-fr/rimo/pkg/model"
 	"github.com/cgi-fr/rimo/pkg/rimo"
+	"github.com/hexops/valast"
 )
 
+// TESTS
+
+func TestTestInterface(t *testing.T) {
+	t.Parallel()
+
+	var _ rimo.Reader = (*TestReader)(nil)
+
+	var _ rimo.Writer = (*LogWriter)(nil)
+}
+
+// Note : numeric value should be converted to float64.
+func TestPipeline(t *testing.T) {
+	t.Parallel()
+
+	// Set up TestReader
+	baseName := "databaseName"
+	tableNames := []string{"tableTest"}
+	testInput := []colInput{
+		{
+			ColName:   "string",
+			ColValues: []interface{}{"val1", "val2", "val3"},
+		},
+		{
+			ColName:   "col2",
+			ColValues: []interface{}{true, false, nil},
+		},
+		{
+			ColName:   "col9",
+			ColValues: []interface{}{float64(31), float64(29), float64(42)},
+		},
+	}
+
+	testReader := TestReader{ //nolint:exhaustruct
+		baseName:   baseName,
+		tableNames: tableNames,
+		data:       testInput,
+		index:      0,
+	}
+
+	// LogWriter
+	testWriter := LogWriter{}
+
+	err := rimo.AnalyseBase(&testReader, &testWriter)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+}
+
+// TestReader implementation
+
+type colInput struct {
+	ColName   string
+	ColValues []interface{}
+}
+
 type TestReader struct {
-	index  int
-	values []interface{}
+	baseName   string
+	data       []colInput
+	tableNames []string // Next() will progressively change tableName
+	// internal
+	index            int
+	currentValues    []interface{}
+	currentColName   string
+	currentTableName string
 }
 
 func (r *TestReader) BaseName() string {
-	return ""
+	return r.baseName
 }
 
 func (r *TestReader) Next() bool {
-	return r.index < len(r.values)
+	if r.index == len(r.data) {
+		log.Println("End of data")
+
+		return false
+	}
+
+	// update tableName
+	if len(r.tableNames) == len(r.data) {
+		r.currentTableName = r.tableNames[r.index]
+	} else {
+		// use a percentage to determine the table name to use from the list
+		percentageComplete := float64(r.index) / float64(len(r.data))
+		expectedTableIndex := percentageComplete * float64(len(r.tableNames))
+		roundedTableIndex := math.Floor(expectedTableIndex)
+		tableNameIndex := int(roundedTableIndex)
+
+		r.currentTableName = r.tableNames[tableNameIndex]
+	}
+
+	r.currentColName = r.data[r.index].ColName
+	r.currentValues = r.data[r.index].ColValues
+	r.index++
+
+	return true
 }
 
 func (r *TestReader) Value() ([]interface{}, string, string, error) {
-	return nil, "", "", nil
+	log.Printf("Processing %s column in %s table", r.currentTableName, r.currentColName)
+
+	return r.currentValues, r.currentColName, r.currentTableName, nil
 }
 
-type TestWriter struct{}
+// LogWritter
 
-func (w *TestWriter) Export(base *rimo.Base) error {
+type LogWriter struct{}
+
+func (w *LogWriter) Export(base *model.Base) error {
+	log.Printf("BASE returned \n \n : %s", valast.String(&base))
+
 	return nil
+}
+
+// ReturnWriter : return the base object through GetBase() method.
+type ReturnWriter struct {
+	base model.Base
+}
+
+func (w *ReturnWriter) Export(base *model.Base) error {
+	w.base = *base
+
+	return nil
+}
+
+func (w *ReturnWriter) GetBase() model.Base {
+	return w.base
 }
--- pkg/rimo/driver.go
+++ pkg/rimo/driver.go
@@ -1,11 +1,19 @@
 package rimo
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/cgi-fr/rimo/pkg/metric"
+	"github.com/cgi-fr/rimo/pkg/model"
+
+	"github.com/hexops/valast"
+)
 
 func AnalyseBase(reader Reader, writer Writer) error {
 	baseName := reader.BaseName()
-
-	base := NewBase(baseName)
+	log.Printf("Processing base : %s", baseName)
+	base := model.NewBase(baseName)
 
 	for reader.Next() { // itère colonne par colonne
 		colValues, colName, tableName, err := reader.Value()
@@ -13,15 +21,21 @@ func AnalyseBase(reader Reader, writer Writer) error {
 			return fmt.Errorf("failed to get column value : %w", err)
 		}
 
-		column, err := Analyse(colValues, colName)
+		log.Printf("Processing %s column in %s table", tableName, colName)
+		log.Printf("Column values : %v", colValues)
+
+		column, err := metric.ComputeMetric(colName, colValues)
 		if err != nil {
-			return fmt.Errorf("failed to compute model.column : %w", err)
+			return fmt.Errorf("failed to compute column : %w", err)
 		}
+
+		fmt.Printf("Column : %s\n", valast.String(column))
 
 		base.AddColumn(column, tableName)
 	}
 
 	base.SortBase()
+	log.Printf("Base processed : %s", valast.String(base))
 
 	err := writer.Export(base)
 	if err != nil {
@@ -29,9 +43,4 @@ func AnalyseBase(reader Reader, writer Writer) error {
 	}
 
 	return nil
-}
-
-// Process a input and return a model.Column.
-func Analyse(colValues []interface{}, colName string) (Column, error) {
-	return Column{}, nil //nolint:exhaustruct
 }
